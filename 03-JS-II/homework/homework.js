e46b3e26067535c37c5c876b1a3255dff9aa163a
J7-B0TT0 <joaquinbotto2@hotmail.com>
2023-01-16 19:23:23 -0300

    Last HW

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -135,7 +135,7 @@ function fizzBuzz(numero) {
   else if (numero % 5 === 0){
     return 'buzz';
   }
-  else if (numero % 3 === 0 || numero % 5 === 0) {
+  else if (numero % 3 === 0 && numero % 5 === 0) {
     return 'fizzbuzz';
   }
   else {
@@ -169,11 +169,20 @@ function operadoresLogicos(num1, num2, num3) {
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
-  // De lo contrario devuelve "falso"
+  // De lo contrario devuelve "false"
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-
+  if (numero < 2) {
+    return false;
+  }
+  else if (numero === 2) {
+    return true;
+  }
+  for (var i = 2; i < numero; i++) {
+    return false;
+  }
+  return true;
 }
 
 function esVerdadero(valor){
