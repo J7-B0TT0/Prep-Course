b9d3cba07c1e4be54722697ea7cde5f54371d6d0
J7-B0TT0 <joaquinbotto2@hotmail.com>
2023-01-23 12:13:47 -0300

    homeWork 23/1

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -173,8 +173,10 @@ function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí 
+  if (numeroDeDia === 1 || numeroDeDia === 7){return 'Es fin de semana'}
+  else return 'Es dia laboral';
+
 } 
 
 
@@ -182,7 +184,7 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+
 }
 
 
