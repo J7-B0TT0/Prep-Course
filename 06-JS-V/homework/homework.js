276dc7cc5b96def5226cf0c00cd28ca08bedd0ed
J7-B0TT0 <joaquinbotto2@hotmail.com>
2023-02-27 18:48:43 -0300

    Last changes

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -1,19 +1,31 @@
 // No cambies los nombres de las funciones.
 
 function crearUsuario() {
-  // Crea una Clase de ES6 o una función constructor llamada "Usuario"
+  // Crea una Clase de ES6 o una función constructora llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
-  // En el `contructor`, define el usuario, el nombre, el email y la contraseña
-  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // En el `constructor`, define el usuario, el nombre, el email y la contraseña
+  // El `constructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario(opciones){
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  }
+
+  Usuario.prototype.saludar=function(){
+  'Hola, mi nombre es ' + this.nombre;
+  }
+  return Usuario; 
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  
 }
 
 function agregarStringInvertida() {
